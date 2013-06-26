a4131e329733f45884631020eefccbb89239bd6e
Andrew <a.a.m.macdonald@gmail.com>
2013-06-26 15:06:52 -0700

    predator diversity modifications and deleted the general.R thing

--- R.scripts/general.R
+++
@@ -1,8 +0,0 @@
-
-
-pngPPT<-function(file,width=10,height=7.5,res=110,single=TRUE){
-	png(file,width,height,units='in',res=res)
-	if(single==TRUE)
-	par(omi=c(0,1.25,0,1.25),bty='l')
-	else
-	par(bty='l')}	
