c5dca3209f5e34de6cdef9b77d4e5904dc4a4f2c
Andrew <a.a.m.macdonald@gmail.com>
2013-07-04 13:29:51 -0700

    some work on the MS, plus the addition of new tree data

--- MS/knit.sh
+++ MS/knit.sh
@@ -3,19 +3,21 @@ cp ~/Documents/Mendeley_reference_lists/predator\ diversity.bib .
 # Make the figure
 # Make the manuscript
 Rscript -e "library(knitr); knit('predatordiversity_source.Rmd')"
-pandoc -s -S --bibliography=predator\ diversity.bib predatordiversity_source.md -o predatordiversity.pdf --latex-engine=xelatex
+pandoc -s -S --bibliography=predator\ diversity.bib predatordiversity_source.md -o predatordiversity_source.pdf --latex-engine=xelatex
 #pandoc -s -S predatordiversity-knitr.md -o predatordiversity-knitr.pdf
 
 ## make the figures
-#Rscript -e "library(knitr); knit('predatordiversity-knitr_figs.Rmd')"
-#pandoc -s -S predatordiversity-knitr_figs.md -o predatordiversity-knitr_figs.pdf
+Rscript -e "library(knitr); knit('predatordiversity_figs.Rmd')"
+pandoc -s -S predatordiversity_figs.md -o predatordiversity_figs.pdf
 
 
 # Combine the two 
-#pdfunite predatordiversity_source.pdf predatordiversity_figs.pdf predatordiversity_MS.pdf
+pdfunite predatordiversity_source.pdf predatordiversity_figs.pdf predatordiversity_MS.pdf
 # Remove duplicate files and rename concated one to original name
-#rm predatordiversity_source.pdf
-#rm predatordiversity_figs.pdf
+rm predatordiversity_source.pdf
+rm predatordiversity_figs.pdf
+rm *.html
+rm *.md
 
 #pandoc -H margins.sty --bibliography git_ms.bib --csl plos.csl git_manuscript.md -o git_manuscript.tex
 #pandoc -H margins.sty list_of_figures.md -o list_of_figures.tex
--- R.scripts/README_rscripts.md
+++ R.scripts/README_rscripts.md
@@ -1,3 +1,7 @@
 # R scripts for pd paper 
 
-* `experimental_data_collate_cleanup.R` is for combining all the different datasets, and cleaning them.  It produces `pd_exp_cleaned_data.csv` which contains *all* the data relevant to the 
+* `experimental_data_collate_cleanup.R` is for combining all the different datasets relevant to the **whole bromeliad experiment**, and cleaning them.  It produces `pd_exp_cleaned_data.csv` which contains *all* the data relevant to the experiment in 2011
+
+* `foodweb_merge_cleanup.R` handles all the feeding trial data, and merges it with the leptagrion data.  the output is saved in `data/reorganized_data/reorganized.feeding.trial.data.csv`
+
+* 
