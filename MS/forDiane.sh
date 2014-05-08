9619d687c530bc7df0f22049a40c4b8be857ffb1
Andrew <a.a.m.macdonald@gmail.com>
2014-05-08 09:24:51 -0700

    experimenting with output formats

--- MS/forDiane.sh
+++ MS/forDiane.sh
@@ -1,8 +1,10 @@
 # copy the latest bibliography from Mendeley's export
-cp ~/Documents/Mendeley_reference_lists/pdpaper.bib ./pdpaper.bibtex
+cp ~/Documents/Mendeley_reference_lists/@MS_pdpaper.bib ./pdpaper.bibtex
 # Make the figure
 # Make the manuscript
 Rscript -e "library(knitr); knit('predatordiversity.Rmd')"
 
 # write out a pretty word doc 
-pandoc  -f markdown+citations+pipe_tables -t docx -H format.sty --reference-docx=reference.docx --bibliography=pdpaper.bibtex predatordiversity.md -o predatordiversity.docx
+pandoc  -f markdown+citations+grid_tables -t docx -H format.sty --reference-docx=reference.docx --bibliography=pdpaper.bibtex predatordiversity.md -o predatordiversity.docx
+
+pandoc  -f markdown+citations+grid_tables -H format.sty --bibliography=pdpaper.bibtex predatordiversity.md -o predatordiversity.pdf
