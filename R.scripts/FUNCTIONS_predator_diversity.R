f7d044068849ef2dbd94e4eac7930c0593729a55
Andrew <a.a.m.macdonald@gmail.com>
2014-06-13 00:20:38 -0700

    nonlinear fit and confint for fig 1 and 2

--- R.scripts/FUNCTIONS_predator_diversity.R
+++ R.scripts/FUNCTIONS_predator_diversity.R
@@ -1,15 +1,146 @@
 ### functions for analyzing predator diversity experiment 2011
 ## June 2011, Andrew MacDonald
 
+
+fit_some_models <- function(data_phylo_overlap){
+
+  
+  models_to_fit <- data.frame(formulae = c(
+    "quadratic" = "overlap ~ a * (phylodistance)^2 + b * phylodistance + c",
+    "bellshaped" = "overlap ~ peak * exp(-1 * (phylodistance)^2 / a)",
+    "exponential" = "overlap ~ b * exp(a * phylodistance)",
+    "Sshaped" = "overlap ~ c * exp(a * phylodistance) / (c * exp(a * phylodistance) + (1 - c))")
+  ) %>% tbl_df()
+  
+  models_to_fit$start <-  list(
+    list(a = -0.0000008, b = 0.0008, c = 1),
+    list(a = 900000, peak = 1),
+    list(a = -0.0004, b = 1),
+    list(a = -0.007, c = 0.9))
+  
+  # class(models_to_fit) <- c("tbl_df", "data.frame")
+  # attr(models_to_fit, "row.names") <- .set_row_names(length(models_to_fit))
+  
+  models <- data_phylo_overlap %>%
+    do(quadratic = nls(overlap ~ a * (phylodistance)^2 + b * phylodistance + c, 
+                       data = ., 
+                       start = list(a = -0.0000008, b = 0.0008, c = 1)),
+       bellshaped = nls(overlap ~ peak * exp(-1 * (phylodistance)^2 / a), 
+                        data = ., 
+                        start = list(a = 900000, peak = 1)),    
+       exponential = nls(overlap ~ b * exp(a * phylodistance),
+                         data = .,
+                         start = list(a = -0.0004, b = 1)),
+#        Sshaped = nls(overlap ~ c * exp(a * phylodistance) / 
+#                        (c * exp(a * phylodistance) + (1 - c)),
+#                      data = .,
+#                      start = list(a = -0.007, c = 0.9)),
+       linear = lm(overlap ~ phylodistance, data = .),
+       constant = nls(overlap ~ z,
+                      data = .,
+                      start = list(z = 0.4))
+    ) 
+  
+  f_extract <- function(f) sapply(models,function(x) f(x[[1]]))
+  
+  data.frame(model = names(models),
+             AIC = f_extract(AIC)) %>%
+    arrange(AIC)
+}
+
+pianka_plot <- function(data_phylo_overlap,formula_quote="overlap ~ peak * exp(-1 * (phylodistance)^2 / a)"){
+  fig1 <- data_phylo_overlap %>%
+    #   group_by(PDgrp) %>%
+    #   mutate(grp_mean = mean(overlap),
+    #          grp_n = n(),
+    #          grp_se = sd(overlap)/sqrt(grp_n),
+    #          PD_mean = mean(phylodistance)) %>%
+    ggplot(aes(x = phylodistance, y = overlap)) + 
+    geom_point(shape  = 21, size = 4) + 
+    geom_point(colour = "black", fill = "darkgrey", size = 4, shape  = 21, alpha = 0.6) + 
+    #   geom_point(aes(x = PD_mean,
+    #                  y = grp_mean,
+    #                  size = grp_n),
+    #              colour = "black", shape = 21) + 
+    #   geom_linerange(aes(x = PD_mean,
+    #                       ymin = grp_mean - grp_se,
+    #                       ymax = grp_mean + grp_se),
+    #                   colour = "black") + 
+    #   scale_size(range = c(3,9),name="Number of\npoints per\nmean") + 
+    xlab("Phylogenetic distance") + ylab("Overlap in habitat use") + 
+    mytheme
+  
+  model_formula <- as.formula(formula_quote)
+  
+  x_sequence <- metabolic_occur_phylo  %>%
+    ungroup %>%
+    summarize(min_x = min(phylodistance),
+              max_x = max(phylodistance)) %>%
+    do(seq(from = .$min_x,
+           to = .$max_x,
+           length.out = 500) %>%
+         data.frame(phylodistance = .)
+    ) 
+  
+  rawdata <- metabolic_occur_phylo %>% 
+    ungroup %>% 
+    select(phylodistance,overlap)
+  
+  predictions  <-  replicate(
+    100,{
+      boot  <- rawdata[sample.int(nrow(rawdata), replace = TRUE), ]
+      model = failwith(NULL,nls)(model_formula, 
+                                 data = boot, 
+                                 start = list(a = 900000, peak = 1))
+      # Output predictions at each point that we'll want to plot later
+      if(!is.null(model)) predict(model, data.frame(x = x_sequence)) else rep(NA,length(x_sequence))
+    }
+  )
+  
+  #  browser()
+  
+  observed_fit <- nls(overlap ~ peak * exp(-1 * (phylodistance)^2 / a), 
+                      data = rawdata, 
+                      start = list(a = 900000, peak = 1))
+  
+  x_sequence %>%
+    mutate(pred_m2 = predict(observed_fit,newdata = list(phylodistance = phylodistance)),
+           upper = apply(predictions, 1, quantile, .975, na.rm = TRUE),
+           lower = apply(predictions, 1, quantile, .025, na.rm = TRUE)
+    ) %>% 
+    #  gather(model,prediction,-phylodistance) %>%
+    (function(x) fig1 + geom_line(aes(x = phylodistance, y = pred_m2),data = x) +
+       geom_line(aes(x = phylodistance, y = upper),data = x,linetype = "dashed") +
+       geom_line(aes(x = phylodistance, y = lower),data = x,linetype = "dashed")
+    ) 
+}
+
+
+
+list_to_df <- function(listfordf){
+  if(!is.list(listfordf)) stop("it should be a list")
+  
+  df <- list(list.element = listfordf)
+  class(df) <- c("tbl_df", "data.frame")
+  attr(df, "row.names") <- .set_row_names(length(listfordf))
+  
+  if (!is.null(names(listfordf))) {
+    df$name <- names(listfordf)
+  }
+  
+  df
+}
+
+
 # This function turns a matrix into a dataframe
 matrix_to_df <- function(matrix_for_df){
   matrix_for_df %>% 
-  melt(as.is=TRUE) %>%                    # as.is important for preventing factors
-  filter(matrix_for_df %>% 
-         upper.tri %>% 
-         melt %>% 
-         extract2("value")
-  )
+    melt(as.is=TRUE) %>%                    # as.is important for preventing factors
+    filter(matrix_for_df %>% 
+             upper.tri %>% 
+             melt %>% 
+             extract2("value")
+    )
 }
 
 ## To make comparisions between pairs of predators, we need a factor
@@ -21,24 +152,25 @@ paired_predator_pianka <- function(pred_x_resource,pred_colname,...){
   taxa_names <- pred_x_resource %>% extract2(pred_colname)
   
   prednames <- taxa_names %>%
-  unique %>%
-  (function(x) set_names(x,x)) %>%
-  (function(x) outer(x,x,paste,sep="_")) %>%
-  matrix_to_df() %>%
-  select(dietpred1=Var1,dietpred2=Var2,dietpredpair=value)
+    unique %>%
+    (function(x) set_names(x,x)) %>%
+    (function(x) outer(x,x,paste,sep="_")) %>%
+    matrix_to_df() %>%
+    select(dietpred1=Var1,dietpred2=Var2,species_pair=value)
   
   taxa_names %>% unique %>% length %>% choose(2) %>%
-  equals(prednames %>% nrow) %>%
-  not %>%
-  if(.) stop(message("The number of rows in output does not match the number of possible pairs"))
+    equals(prednames %>% nrow) %>%
+    not %>%
+    if(.) stop(message("The number of rows in output does not match the number of possible pairs"))
+  
   ## next merge with the actual data
   prednames %>%
-  melt(id.vars = "dietpredpair",value.name=pred_colname) %>%  ## giving same name here for merging later
-  mutate(dietpredpair=as.character(dietpredpair)) %>% 
-  left_join(pred_x_resource) %>% 
-  select(-variable,...) %>%
-  group_by(dietpredpair) %>%
-  do(. %>% select(-dietpredpair) %>% pianka)
+    melt(id.vars = "species_pair",value.name=pred_colname) %>%  ## giving same name here for merging later
+    mutate(species_pair=as.character(species_pair)) %>% 
+    left_join(pred_x_resource) %>% 
+    select(-variable,...) %>%
+    group_by(species_pair) %>%
+    do(. %>% select(-species_pair) %>% pianka)
 }
 ## then we calculate similarity for each.  for the predation data, we must first remove animals not assayed with both predators. the `pianka` function does this by removing those with NA colSums.
 
@@ -50,11 +182,11 @@ paired_predator_pianka <- function(pred_x_resource,pred_colname,...){
 pianka <- function(df){
   ## remove resources never tested on both animals
   mat <- df %>% 
-  as.matrix %>%
-  colSums %>% 
-  is.na %>%
-  not %>%
-  df[.]
+    as.matrix %>%
+    colSums %>% 
+    is.na %>%
+    not %>%
+    df[.]
   
   # scale rows
   rowtotal <- rowSums(mat)
