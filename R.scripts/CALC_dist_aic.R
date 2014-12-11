4949022e9f2a2600392d67e476a8c21e542505a2
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-12-11 02:21:54 -0800

    AIC values for various regressions of the distributional similarity stuff

---
+++ R.scripts/CALC_dist_aic.R
@@ -0,0 +1,15 @@
+### Distribution
+## calculates the AIC values that appear in the supp matt.
+
+metabolic_occur_phylo <- read.csv("../data/metabolic_occur_phylo.csv",
+                                  stringsAsFactors = FALSE)
+## load in functions
+source("../R.scripts/FUNCTIONS_predator_diversity.R")
+
+
+occur_aic <- metabolic_occur_phylo %>% 
+  ungroup %>% 
+  select(phylodistance,overlap) %>%
+  fit_some_models
+
+write.csv(occur_aic,file = "../data/distributional_similarity.csv")
