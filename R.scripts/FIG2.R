caab8f7f8bbc02602705a4b18924f79e6504b316
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-12-12 12:45:13 -0800

    minor edits to figure 2. Sentence case and better y axis labels

--- R.scripts/FIG2.R
+++ R.scripts/FIG2.R
@@ -7,7 +7,8 @@ require("gridExtra")
 
 ## ggplot2 theme
 mytheme <- theme_bw() + theme(strip.text = element_text(hjust = 0.01),
-                              strip.background = element_blank())
+                              strip.background = element_blank(),
+                              axis.title.y = element_blank())
 
 one_point <- geom_point(colour = "black", 
                         fill = NA, shape = 21, colour = "black", 
@@ -29,7 +30,7 @@ pred_present <- pd %>%
   select(treatment, total.surv) %>%
   group_by(treatment) %>%
   summarise(trtmeans = mean(total.surv)) %>%
-  mutate(ispred = ifelse(treatment == "control", "absent", "present")) %>%
+  mutate(ispred = ifelse(treatment == "control", "Absent", "Present")) %>%
   group_by(ispred) %>%
   mutate(presence_mean = mean(trtmeans))
 
@@ -40,7 +41,7 @@ pred_present_plot <- pred_present %>%
   geom_point(aes(y = presence_mean), fill = "#00A08A", shape = 21, colour = "black", size = 5) + 
   #   geom_point(aes(y = total.surv), fill = NA, shape = 21, colour = "black", size = 5,
   #              data = subset(pred_present, treatment == "control")) + 
-  ylab("Mean prey survival") + xlab("Predator presence") + 
+  xlab("Predator presence") + 
   facet_grid(~ plotcode) +
   mytheme
 
@@ -49,27 +50,47 @@ pred_present_plot <- pred_present %>%
 
 
 pred_number <- pred_present %>% 
-  filter(ispred == "present") %>%
-  mutate(npred = ifelse(grepl(" \\+ ", treatment), "two", "one")) 
+  filter(ispred == "Present") %>%
+  mutate(npred = ifelse(grepl(" \\+ ", treatment), "Two", "One")) 
 
 pred_number_plot <- pred_number %>%
   mutate(plotcode = "(b)") %>%
   ggplot(aes(x = npred, y = trtmeans)) +
   one_point + 
   stat_summary(fun.y = mean, fill = "#00A08A", shape = 21, size = 5, geom = "point") +
-  ylab("Mean survival (n = 5)") + xlab("Predator number") + 
+  xlab("Predator number") + 
   facet_grid(~ plotcode) +
-  mytheme + theme(axis.title.y = element_blank())
+  mytheme
 
 
 # identity ----------------------------------------------------------------
 
+releveler <- data_frame(treatment = c("andro", 
+                         "tabanid", 
+                         "elong + andro", 
+                         "control", 
+                         "leech", 
+                         "elong + leech", 
+                         "elong", 
+                         "elong + tab"),
+                        newlevels = c(
+                          "La", 
+                          "Tabanid", 
+                          "Le + La", 
+                          "control", 
+                          "Leech",
+                          "Le + Leech",
+                          "Le",
+                          "Le + Tabanid"))
+
+
 
 preds <- pd %>%
   select(treatment, total.surv) %>%
-  mutate(treatment = factor(treatment, 
-                            levels = c("andro","elong","tabanid","leech",
-                                       "elong + andro", "elong + tab", "elong + leech"))) %>%
+  left_join(releveler) %>%
+  mutate(treatment = factor(newlevels, 
+                            levels = c("La","Le","Tabanid","Leech",
+                                       "Le + La", "Le + Tabanid", "Le + Leech"))) %>%
   filter(treatment != "control") %>% 
   mutate(npred = ifelse(grepl(" \\+ ", treatment),
                         "Two species combination", "Monoculture"))
@@ -83,7 +104,6 @@ pred_identity_plot <- pred_identity %>%
   one_point + 
   stat_summary(fun.y = mean, fill = "#00A08A", shape = 21, size = 5, geom = "point") +
   xlab("Predator identity") +
-  ylab("Mean prey survival") +
   facet_grid(~ plotcode) +
   mytheme
 
@@ -101,10 +121,11 @@ pred_combo_plot <- pred_combo %>%
   stat_summary(fun.y = mean, fill = "#00A08A", shape = 21, size = 5, geom = "point") +
   xlab("Predator combination") +
   facet_grid(~ plotcode) + 
-  mytheme + theme(axis.title.y = element_blank())
+  mytheme
 
-
-
-grid.arrange(pred_present_plot, pred_number_plot, pred_identity_plot, pred_combo_plot, ncol=2)
-
-ggsave("../Figures/FIG_2.pdf", height = 6, width = 6, units = "in")
+  
+pdf("../Figures/FIG_2.pdf", height = 6, width = 6)
+grid.arrange(pred_present_plot, pred_number_plot, pred_identity_plot, pred_combo_plot,
+             ncol=2,
+             left = textGrob("Mean prey survival", rot = 90, vjust = 1))
+dev.off()
