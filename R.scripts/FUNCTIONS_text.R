1f8e59576b5648ff90f05c68c0d6c8c5b620e994
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-12-18 09:26:16 -0800

    functions out of .Rmd to own file

--- R.scripts/FUNCTIONS_text.R
+++ R.scripts/FUNCTIONS_text.R
@@ -38,3 +38,38 @@ F_text <- function(model_summary){
   paste0("F~", model_summary$fstatistic[["numdf"]], ",", model_summary$fstatistic[["dendf"]],
          "~=", fval, ", p=", pval)
 }
+
+## Extract pretty Fvals from the `modlist` object, which is a list of models.
+prF <- function(resp,test,.modlist = modlist){
+  modsum <- .modlist[[test]] %>%
+    filter(response == resp) %>%
+    extract2("m") %>%
+    extract2(1) %>%
+    summary 
+  
+  fv <- modsum %>%
+    extract2("fstatistic")
+  F_format <- paste0("F~",fv[["numdf"]],",",fv[["dendf"]],"~", " = ")
+  pv <- pf(fv[1],fv[2],fv[3],lower.tail=F)
+  
+  fval <- round(fv[["value"]], 2)
+  
+  if (pv < 0.05) {
+    paste0(F_format,"**",fval,"**")
+  } else {
+    paste0(F_format,fval)      
+  }
+}
+
+## polyculture effect sizes
+polyeffect <- function(resp="total.surv"){
+  diffeffect <- (mean(pd[[resp]][pd$treatment%in%c("elong + andro","elong + leech","elong + tab")],na.rm=TRUE)-mean(pd[[resp]][pd$treatment%in%c("andro","tabanid","leech","elong")],na.rm=TRUE))/mean(pd[[resp]][pd$treatment=="control"],na.rm=TRUE)
+  round(diffeffect,digits=2)*100
+}
+
+## predator effect sizes
+predeffect <- function(resp="total.surv"){
+  diffeffect <- (mean(pd[[resp]][pd$treatment!="control"],na.rm=TRUE)-mean(pd[[resp]][pd$treatment=="control"],na.rm=TRUE))/mean(pd[[resp]][pd$treatment=="control"],na.rm=TRUE)
+  round(diffeffect,digits=2)*100
+}
+
