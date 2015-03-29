14a0223db7b50009432a38c669cfd0cc48357393
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-03-29 10:32:20 -0700

    writing out figure 3

--- R.scripts/FIG3.R
+++ R.scripts/FIG3.R
@@ -6,13 +6,14 @@ library("dplyr")
 summarize_randoms_phylo <- read.csv("../data/summarize_randoms_phylo.csv")
 
 summarize_randoms_phylo %>% 
-  filter(response_var == "survival") %>%
+  filter(response_var == "N") %>%
   ggplot(aes(x=phylodistance,y=mean)) + 
   geom_linerange(aes(ymin=lower, ymax=upper)) + 
-  geom_point(size=6,shape = 21, colour = "black", fill = "#00A08A") + 
+  geom_point(size=6, shape = 21, colour = "black", fill = "#00A08A") + 
   ylab("Surviving prey species (nonadditive effect)") + 
   xlab("Time (Mya)") + 
   geom_hline(yintercept = 0, linetype = "dashed", colour = "grey") + 
-  theme_bw()
+  theme_minimal() +   
+  theme(axis.ticks = element_blank(), text = element_text(size=20))
 
-ggsave("../Figures/FIG_3.pdf", height = 6, width = 6, units = "in")
+ggsave("../Figures/FIG_3_N.png", height = 21, width = 28, units = "cm")
