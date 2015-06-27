7ee36e1bf7fc6d9f9d0552fae657ce01c216855e
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-06-27 14:36:57 -0700

    functions for fig1 and 3. #4

--- R.scripts/FIG1.R
+++ R.scripts/FIG1.R
@@ -1,85 +1,67 @@
 
-library("dplyr")
-library("magrittr")
-library("reshape2")
-library("ggplot2")
+pd_exper_responses <- function(.pd){
+  .pd %>%
+    filter(treatment %in% c("andro","elong","tabanid","leech")) %>%
+    select(treatment, Culicidae:Scirtidae) %>%
+    group_by(treatment) %>%
+    summarise_each(funs(mean)) %>%
+    paired_predator_pianka(pred_colname = "treatment", ... = -treatment) %>%
+    ungroup %>%
+    mutate(species_pair = ifelse(species_pair == "andro_elong",
+                                 "Leptagrion.andromache_Leptagrion.elongatum",
+                                 species_pair),
+           species_pair = ifelse(species_pair == "andro_leech",
+                                 "Leptagrion.andromache_Hirudinidae",
+                                 species_pair),
+           species_pair = ifelse(species_pair == "andro_tabanid",
+                                 "Leptagrion.andromache_Tabanidae.spA",
+                                 species_pair),
+           species_pair = ifelse(species_pair == "elong_leech",
+                                 "Leptagrion.elongatum_Hirudinidae",
+                                 species_pair),
+           species_pair = ifelse(species_pair == "elong_tabanid",
+                                 "Leptagrion.elongatum_Tabanidae.spA",
+                                 species_pair),
+           species_pair = ifelse(species_pair == "leech_tabanid",
+                                 "Tabanidae.spA_Hirudinidae",
+                                 species_pair)) %>%
+    left_join(phylogenetic_distance)
+}
 
-source("../R.scripts/FUNCTIONS_predator_diversity.R")
-
-## ggplot2 theme
-mytheme <- theme_bw()
-
-
-phylogenetic_distance  <- read.csv("../data/phylogenetic_distance.csv", stringsAsFactors = FALSE)
-
-
-metabolic_occur_phylo <- read.csv("../data/metabolic_occur_phylo.csv",
-                                  stringsAsFactors = FALSE)
-
-diet_overlap_phylo <- read.csv("../data/diet_overlap_phylo.csv",
-                               stringsAsFactors=FALSE) %>%   tbl_df()
-
-# experimental data
-pd <- read.csv("../data/pd_exp_cleaned_data.csv",
-               stringsAsFactors=FALSE) %>%   tbl_df()
-
-
-diet_predictions <- read.csv("../data/diet_predictions.csv", stringsAsFactors = FALSE)
-
-distribution <- metabolic_occur_phylo %>% 
-  ungroup %>% 
-  select(phylodistance,overlap) %>%
-  mutate(category = "(a)", nspp = 2)
-
-diet <- diet_overlap_phylo %>% 
-  ungroup %>%
-  select(phylodistance,overlap,nspp) %>%
-  mutate(category = "(b)") %>%
-  as.data.frame
-
-exper <- pd %>%
-  filter(treatment %in% c("andro","elong","tabanid","leech")) %>%
-  select(treatment, Culicidae:Scirtidae) %>%
-  group_by(treatment) %>%
-  summarise_each(funs(mean)) %>%
-  paired_predator_pianka(pred_colname = "treatment", ... = -treatment) %>%
-  ungroup %>%
-  mutate(species_pair = ifelse(species_pair == "andro_elong",
-                               "Leptagrion.andromache_Leptagrion.elongatum", species_pair),
-         species_pair = ifelse(species_pair == "andro_leech",
-                               "Leptagrion.andromache_Hirudinidae", species_pair),
-         species_pair = ifelse(species_pair == "andro_tabanid",
-                               "Leptagrion.andromache_Tabanidae.spA", species_pair),
-         species_pair = ifelse(species_pair == "elong_leech",
-                               "Leptagrion.elongatum_Hirudinidae", species_pair),
-         species_pair = ifelse(species_pair == "elong_tabanid",
-                               "Leptagrion.elongatum_Tabanidae.spA", species_pair),
-         species_pair = ifelse(species_pair == "leech_tabanid",
-                               "Tabanidae.spA_Hirudinidae", species_pair)) %>%
-  left_join(phylogenetic_distance) %>% 
-  ungroup %>%
-  select(phylodistance,overlap,nspp) %>%
-  mutate(category = "(c)")
-
-### write out the experiment result for use in the result section
-save(exper, file = "../data/exper.Rdata")
+make_fig_1 <- function(.metabolic_occur_phylo, .diet_overlap_phylo,
+                       .experiment_phylo, .diet_predictions){
+  
+  distribution <- .metabolic_occur_phylo %>% 
+    ungroup %>% 
+    select(phylodistance,overlap) %>%
+    mutate(category = "(a)", nspp = 2)
+  
+  diet <- .diet_overlap_phylo %>% 
+    ungroup %>%
+    select(phylodistance,overlap,nspp) %>%
+    mutate(category = "(b)") %>%
+    as.data.frame
+  
+  exper <- .experiment_phylo %>% 
+    ungroup %>%
+    select(phylodistance,overlap,nspp) %>%
+    mutate(category = "(c)")
 
 
-rbind(distribution, diet, exper) %>%
-  ggplot(aes(x = phylodistance, y = overlap, size = nspp)) + 
-  geom_point(colour = "black", fill = "#00A08A",
-             shape  = 21, alpha = 0.6) +
-  facet_wrap( ~ category, scales = "free_y", nrow = 3) + 
-  xlab("Phylogenetic distance") + 
-  ylab("Similarity (Pianka's index)") + 
-  scale_size(range = c(3,9),name = "Number of\nprey") +
-  geom_line(aes(x = phylodistance, y = pred_m2), 
-            size = 0.5, data = diet_predictions) +
-  geom_line(aes(x = phylodistance, y = upper),
-            size = 0.5, data = diet_predictions, linetype = "dashed") +
-  geom_line(aes(x = phylodistance, y = lower),
-            size = 0.5, data = diet_predictions, linetype = "dashed") + 
-  mytheme + theme(strip.text = element_text(hjust = 0.01),
-                  strip.background = element_blank())
-
-ggsave("../Figures/FIG_1.png", height = 6, width = 6, units = "in")
+  rbind(distribution, diet, exper) %>%
+    ggplot(aes(x = phylodistance, y = overlap, size = nspp)) + 
+    geom_point(colour = "black", fill = "#00A08A",
+               shape  = 21, alpha = 0.6) +
+    facet_wrap( ~ category, scales = "free_y", nrow = 3) + 
+    xlab("Phylogenetic distance") + 
+    ylab("Similarity (Pianka's index)") + 
+    scale_size(range = c(3,9),name = "Number of\nprey") +
+    geom_line(aes(x = phylodistance, y = pred_m2), 
+              size = 0.5, data = .diet_predictions) +
+    geom_line(aes(x = phylodistance, y = upper),
+              size = 0.5, data = .diet_predictions, linetype = "dashed") +
+    geom_line(aes(x = phylodistance, y = lower),
+              size = 0.5, data = .diet_predictions, linetype = "dashed") + 
+    mytheme + theme(strip.text = element_text(hjust = 0.01),
+                    strip.background = element_blank())
+}
--- R.scripts/FIG3.R
+++ R.scripts/FIG3.R
@@ -1,19 +1,13 @@
 ## Generate Fig 3 from summarized randomization test results
 
-library("ggplot2")
-library("dplyr")
-
-summarize_randoms_phylo <- read.csv("../data/summarize_randoms_phylo.csv")
-
-summarize_randoms_phylo %>% 
-  filter(response_var == "survival") %>%
-  ggplot(aes(x=phylodistance,y=mean)) + 
-  geom_linerange(aes(ymin=lower, ymax=upper)) + 
-  geom_point(size=6, shape = 21, colour = "black", fill = "#00A08A") + 
-  ylab("Surviving prey species (nonadditive effect)") + 
-  xlab("Time (Mya)") + 
-  geom_hline(yintercept = 0, linetype = "dashed", colour = "grey") + 
-  theme_minimal() +   
-  theme(axis.ticks = element_blank(), text = element_text(size=20))
-
-ggsave("../Figures/FIG_3.png", height = 21, width = 28, units = "cm")
+make_fig_3 <- function(.summarize_randoms_phylo){
+  summarize_randoms_phylo %>% 
+    filter(response_var == "survival") %>%
+    ggplot(aes(x=phylodistance,y=mean)) + 
+    geom_linerange(aes(ymin=lower, ymax=upper)) + 
+    geom_point(size=6, shape = 21, colour = "black", fill = "#00A08A") + 
+    ylab("Surviving prey species (nonadditive effect)") + 
+    xlab("Time (Mya)") + 
+    geom_hline(yintercept = 0, linetype = "dashed", colour = "grey") + 
+    mytheme
+}
