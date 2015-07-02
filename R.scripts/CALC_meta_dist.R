39a453cc6dd7cdc4fc62595b8554ea8473cc2cbd
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-07-02 09:42:20 -0700

    transform this script into a function to set default to pianka calculator

--- R.scripts/CALC_meta_dist.R
+++ R.scripts/CALC_meta_dist.R
@@ -1,23 +1,8 @@
-library("dplyr")
-library("magrittr")
-library("reshape2")
 
-source("../R.scripts/FUNCTIONS_predator_diversity.R")
-
-# occurrance data -- metabolic capacity
-metabolic  <- read.csv("../data/predator.cooccur.metabolic.txt",
-                       stringsAsFactors = FALSE) %>%  tbl_df()
-
-phylogenetic_distance  <- read.csv("../data/phylogenetic_distance.csv", stringsAsFactors = FALSE)
-
-
-metabolic_distance <- paired_predator_pianka(pred_x_resource = metabolic,
-                                             pred_colname = "Taxa",... =-Taxa)
-
-metabolic_occur_phylo <-  left_join(metabolic_distance, phylogenetic_distance)
-
-write.csv(metabolic_occur_phylo, file = "../data/metabolic_occur_phylo.csv", row.names = FALSE)
-
-
+make_metabolic_dist_pianka <- function(.metabolic){
+  paired_predator_pianka(pred_x_resource = .metabolic,
+                         pred_colname = "Taxa",
+                         ... =-Taxa)
+}
 
 
