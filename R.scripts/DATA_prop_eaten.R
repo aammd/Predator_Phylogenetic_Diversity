4024f7d44385e691c6e3c7085f2edfbce7c21e76
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-07-02 09:09:21 -0700

    diet proportion stuff into functions, and adding a function to set default arguments for paired_predator_pianka

--- R.scripts/DATA_prop_eaten.R
+++ R.scripts/DATA_prop_eaten.R
@@ -1,23 +1,21 @@
 ## When calculating similiarity, we start with a predator (row) by resource (column) dataframe.
 # pred x prey matrix, with cells = proportion eate
 
-library("dplyr")
-library("tidyr")
+calc_prop_eaten <- function(.feeding_trials){
+  prop.eaten <- .feeding_trials %>% 
+    filter(!Prey.species %in% c("Leptagrion andromache <5mm",
+                                "Leptagrion.elongatum")) %>% 
+    filter(!predator.names %in% c("Phylloicus bromeliarum", "Dysticid")) %>%
+    mutate(prop.eaten = eaten/number.trials) %>%
+    arrange(predator.names,Prey.species) %>%
+    select(-number.trials,-eaten) %>%
+    spread(Prey.species,prop.eaten,fill = NA)
+}
 
-# feeding_trials %>% arrange(predator.names) %>% mutate(prop.eaten=eaten/number.trials)
-feeding_trials <- read.csv("../data/reorganized.feeding.trial.data.csv",
-                           stringsAsFactors=FALSE) %>%   tbl_df()
+make_prop_eaten_pianka <- function(prop.eaten){
+  paired_predator_pianka(pred_x_resource = prop.eaten,
+                         pred_colname = "predator.names",
+                         -predator.names)
+}
 
 
-
-prop.eaten <- feeding_trials %>% 
-  filter(!Prey.species %in% c("Leptagrion andromache <5mm",
-                              "Leptagrion.elongatum")) %>% 
-  filter(!predator.names %in% c("Phylloicus bromeliarum", "Dysticid")) %>%
-  mutate(prop.eaten=eaten/number.trials) %>%
-  arrange(predator.names,Prey.species) %>%
-  select(-number.trials,-eaten) %>%
-  spread(Prey.species,prop.eaten,fill = NA)
-#dcast(predator.names~Prey.species,value.var="prop.eaten",fun.aggregate = mean)
-
-write.csv(prop.eaten,"../data/proportion.eaten.csv")
