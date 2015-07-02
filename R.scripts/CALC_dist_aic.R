31e55bad606c18d09c7ac0f8cda478c8751a1b18
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-07-02 09:51:32 -0700

    convert CALC_dist_aic.R to function. output appears unused!!

--- R.scripts/CALC_dist_aic.R
+++ R.scripts/CALC_dist_aic.R
@@ -1,15 +1,6 @@
-### Distribution
-## calculates the AIC values that appear in the supp matt.
-library("dplyr")
-metabolic_occur_phylo <- read.csv("../data/metabolic_occur_phylo.csv",
-                                  stringsAsFactors = FALSE)
-## load in functions
-source("../R.scripts/FUNCTIONS_predator_diversity.R")
-
-
-occur_aic <- metabolic_occur_phylo %>% 
-  ungroup %>% 
-  select(phylodistance,overlap) %>%
-  fit_some_models
-
-write.csv(occur_aic,file = "../data/distributional_similarity_AIC.csv", row.names = FALSE)
+make_dist_sim_AIC <- function(.metabolic_occur_phylo){
+  .metabolic_occur_phylo %>% 
+    ungroup %>% 
+    select(phylodistance,overlap) %>%
+    fit_some_models
+}
