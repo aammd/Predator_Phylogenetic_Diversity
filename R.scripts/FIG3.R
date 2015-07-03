8c79e771d88de44d0e8669cb738317cc9b47ad37
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-07-03 11:50:06 -0700

    formatting and renaming

--- R.scripts/FIG3.R
+++ R.scripts/FIG3.R
@@ -2,10 +2,10 @@
 
 make_fig_3 <- function(.summarize_randoms_phylo){
   .summarize_randoms_phylo %>% 
-    filter(response_var == "survival") %>%
-    ggplot(aes(x=phylodistance,y=mean)) + 
-    geom_linerange(aes(ymin=lower, ymax=upper)) + 
-    geom_point(size=6, shape = 21, colour = "black",
+    filter(response == "total.surv") %>%
+    ggplot(aes(x = phylodistance, y = value)) + 
+    geom_linerange(aes(ymin = lower, ymax = upper)) + 
+    geom_point(size = 6, shape = 21, colour = "black",
                fill = "#00A08A") + 
     theme_bw() + 
     ylab("Surviving prey species (nonadditive effect of predation)") + 
