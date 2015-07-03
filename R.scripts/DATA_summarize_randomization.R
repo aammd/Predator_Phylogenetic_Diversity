fbb45587addf591beb73c7c044e27843a4e2c96b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-07-03 06:29:18 -0700

    formatting errors in this function

--- R.scripts/DATA_summarize_randomization.R
+++ R.scripts/DATA_summarize_randomization.R
@@ -4,20 +4,19 @@ make_randomization_summary <- function(.rand.means){
                    "elong + tab" = "Leptagrion.elongatum_Tabanidae.spA",
                    "elong + leech" = "Leptagrion.elongatum_Hirudinidae")
   
-  rand.means %>% 
+  .rand.means %>% 
     # remove annoying X column
-    select(-X) %>%
+    #select(-X) %>%
     # melt, so that all responses can be summarized at the same time
     gather(response_var,value,-sp.pair) %>% 
     group_by(sp.pair,response_var) %>% 
-    summarise(mean=mean(value),
-              lower=quantile(value,probs=c(0.025)),
-              upper=quantile(value,probs=c(0.975))
+    summarise(mean = mean(value),
+              lower = quantile(value,probs=c(0.025)),
+              upper = quantile(value,probs=c(0.975))
     ) %>%
     # sequence of increasing PD
     ungroup() %>%
-    mutate(species_pair = betternames[sp.pair] %>% as.character) %>%
-    left_join(phylogenetic_distance)
+    mutate(species_pair = betternames[sp.pair] %>% as.character)
 }
   
 
