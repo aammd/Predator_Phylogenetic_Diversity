ee3a991b879717b7fd106a750cd0fe563a05c86e
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-07-15 17:38:15 -0700

    nonedits to DATA_nodeages.R :sweat_smile:

--- R.scripts/DATA_nodeages.R
+++ R.scripts/DATA_nodeages.R
@@ -1,13 +1,13 @@
 library("dplyr")
 library("magrittr")
 
-list.files(path="../raw-data/TreeData",
-                       pattern="*.csv",
-                       full.names=TRUE) %>%
+list.files(path = "../raw-data/TreeData",
+                       pattern = "*.csv",
+                       full.names = TRUE) %>%
   data.frame(paths = .,stringsAsFactors = FALSE) %>%
   mutate(nodename = basename(paths) %>% gsub(".csv","",.)) %>%
   group_by(nodename) %>%
-  do(age_data = read.csv(.$paths)) %>%
+  do(age_data = read.csv(.$paths)) %>% 
   mutate(Nstudies = nrow(age_data),
          minT = min(age_data %>% extract2("Time")),
          maxT = max(age_data %>% extract2("Time"))) %>%
