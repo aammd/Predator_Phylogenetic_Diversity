b117d1319770c818cfe360bbfdba186e489dc859
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-08-28 15:27:54 -0700

    the theme -- corrected to remove that distressing warning and also to give me back my yaxes and restore me to dignity

--- R.scripts/FIGURE_themes.R
+++ R.scripts/FIGURE_themes.R
@@ -1,9 +1,10 @@
 ## ggplot2 theme
-mytheme <- ggplot2::theme_bw() + ggplot2::theme(strip.text = ggplot2::element_text(hjust = 0.01),
+mytheme <- ggplot2::theme_bw() + 
+  ggplot2::theme(strip.text = ggplot2::element_text(hjust = 0.01),
                               strip.background = ggplot2::element_blank(),
-                              axis.title.y = ggplot2::element_blank(),
+                              #axis.title.y = ggplot2::element_blank(),
                               axis.text.x = ggplot2::element_text(size = 7))
 
 one_point <- ggplot2::geom_point(colour = "black", 
-                        fill = NA, shape = 21, colour = "black", 
+                        fill = NA, shape = 21,
                         size = 5, position = ggplot2::position_jitter(w = 0.1))
