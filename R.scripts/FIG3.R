f658dffb0d79fa7b15853d18e82403e52649c54c
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-09-01 22:45:13 -0700

    adding the theme to FIG3

--- R.scripts/FIG3.R
+++ R.scripts/FIG3.R
@@ -1,13 +1,13 @@
 ## Generate Fig 3 from summarized randomization test results
 
-make_fig_3 <- function(.summarize_randoms_phylo){
+make_fig_3 <- function(.summarize_randoms_phylo, .mytheme){
   .summarize_randoms_phylo %>% 
     filter(response == "total.surv") %>%
     ggplot(aes(x = phylodistance, y = value)) + 
     geom_linerange(aes(ymin = lower, ymax = upper)) + 
     geom_point(size = 6, shape = 21, colour = "black",
                fill = "#00A08A") + 
-    theme_bw() + 
+    .mytheme + 
     ylab("Surviving prey species (nonadditive effect of predation)") + 
     xlab("Phylogenetic distance (branch lengths)") + 
     geom_hline(yintercept = 0, linetype = "dashed", colour = "grey")
