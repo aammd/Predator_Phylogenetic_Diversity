0cda2c99e043f0e7efe61c4c62e2e3289e0fae94
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2015-11-29 03:52:14 -0800

    voila, its a mantel test. this one actually works. would probably be better to do this via picante::comm.phylo.cor

--- R.scripts/mantel.R
+++ R.scripts/mantel.R
@@ -1,32 +1,64 @@
 remake::dump_environment()
-diet_overlap_phylo
 
-View(diet_overlap_phylo)
+## to relate the predator phylogenetic distance to the diet or dist similarity we need to do a mantel test
 
-?mantel
+##  diet measurements: ## ok the diet measurements are just a
+#bit more involved, because they are not in the format of a matrix and they
+#never were a matrix
 
-## need to make the phylopred2 into a column
-
-diet_overlap_phylo %>% ungroup %>% 
-  select(phylopred1, phylopred2, phylodistance) %>% 
-  spread(phylopred2, phylodistance)
-
-## distance matrix
-predtree_cophen <- predtree_timetree_ages %>% cophenetic()
-class(predtree_cophen)
-
-spred_diet <- diet_overlap %>% 
-  separate(species_pair, c("sp1", "sp2"), sep = "_") %>% 
+## OK so first step is to turn it into a matrix
+## spreading data.frame
+meta_spread <- metabolic_distance %>% 
+  separate(species_pair, into = c("sp1", "sp2"), sep = "_") %>% 
   select(-nspp) %>% 
   spread(sp2, overlap)
 
-rownames(spred_diet) <- spred_diet[["sp1"]]
+## ugh, converting to matrix strips rownames! why is that sensible?
+meta_mat <- as.matrix(meta_spread[-1])
 
-spred_diet %>% 
-  select(-sp1) %>% 
-  as.matrix %>% 
-#   cbind(matrix(NA, 7 , 1, dimnames = list(NULL, "Hirudinidae")),
-#         .) %>% 
-  t %>% 
-  as.dist
-firef
+row.names(meta_mat) <- meta_spread[["sp1"]]
+
+
+# rank the rows in descending order of non-NA values
+sortrows <- order(rowSums(!is.na(meta_mat)), decreasing = FALSE)
+## rank columns the same way
+sortcols <- order(colSums(!is.na(meta_mat)), decreasing = TRUE)
+
+## ok this matrix looks good
+sortedmat <- meta_mat[sortrows,sortcols]
+## this matches the printed values.
+## the only problem is that when we convert to a distance matrix it doesn't work the way we want it to.
+## it chops off the diagonal, which in this case is needed.
+
+## let me try making a better-behaved matrix. 
+## start with an NA matrix, with the correct rownames
+## ie same rownames as this, but add the first colname to the rows and the last rowname to the end of the cols:
+newmat <- matrix(NA, 14, 14)
+dimnames(newmat) <- list(
+  c(
+    colnames(sortedmat)[1],
+    rownames(sortedmat)),
+  c(
+    colnames(sortedmat),
+    rownames(sortedmat)[13]
+  )
+)
+
+## then just slap in our values:
+newmat[lower.tri(newmat)] <- sortedmat[lower.tri(sortedmat, diag = TRUE)]
+
+newmat_dist <- as.dist(newmat)
+
+
+## first step: get the phylogenetic data in the right format:
+
+
+pdcophen <- cophenetic(predtree_timetree_ages)
+
+intersect(colnames(pdcophen), colnames(newmat))
+
+pd_dist <- as.dist(pdcophen[rownames(newmat), colnames(newmat)])
+
+mantel(pd_dist, newmat_dist)
+
+
