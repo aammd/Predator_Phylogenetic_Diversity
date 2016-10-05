55545319349164951664082a7879374f3d9f5317
Unknown <a.a.m.macdonald@gmail.com>
2016-10-05 14:56:40 +0200

    read a published newick tree

--- R.scripts/READING_data_published.R
+++ R.scripts/READING_data_published.R
@@ -5,3 +5,10 @@ read_published_data <- function(data_id){
   
   read_csv(url, comment = "#")
 }
+
+
+read_published_newick <- function(data_id){
+  url <- fs_download(data_id)
+  
+  read.tree(url)
+}
